98b0065de7b22d6c573923c0380994f03ccd7d2f
poomsrn <befuddlement.live@gmail.com>
2022-12-12 01:58:35 +0700

    [Fix]: WaterSpray

--- Assets/Scripts/Extinguisher.cs
+++ Assets/Scripts/Extinguisher.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Extinguisher : MonoBehaviour
 {
     [SerializeField] private float amountExinguishedPerSecond = 1.0f;
+    public ParticleSystem ps;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +16,9 @@ public class Extinguisher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f)
+       if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, 100f)
         && hit.collider.TryGetComponent(out Fire fire)){
             fire.TryExtinguish(.1f * Time.deltaTime);
-        
         }
     }
 }
